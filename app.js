961d05e8ee257a11ae4d4544908214e9e5ee9a60
DERULO <derumanga@gmail.com>
2026-01-31 04:07:23 -0800

    backend_pwa_Version 2 for update index.js

--- app.js
+++ app.js
@@ -49,13 +49,25 @@ ___________________________________________________________________
 
  */
 
-
-app.use(cors({
-  origin: "https://client-pwa.onrender.com", // ou 3000 selon votre port React
+// origin: "https://client-pwa.onrender.com", // ou 3000 selon votre port React
+/* app.use(cors({
+  origin: "http://localhost:5173",
   credentials: true, // IMPORTANT: autoriser les cookies
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
 }));
+ */
+
+
+
+app.use(cors({
+  origin: ["http://localhost:5173", "https://client-pwa.onrender.com", "https://mon-site.com"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+
 
 
 // Routes

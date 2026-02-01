77783959aa92338529886091c588b097bae4fedf
DERULO <derumanga@gmail.com>
2026-02-01 02:23:19 -0800

    version 5

--- app.js
+++ app.js
@@ -60,7 +60,8 @@ ___________________________________________________________________
 
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://client-pwa.onrender.com", "https://mon-site.com"],
+  origin: ["http://localhost:5173","http://localhost:5174", "https://client-pwa.onrender.com", 
+    "https://mon-site.com", "http://localhost:4173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"]
